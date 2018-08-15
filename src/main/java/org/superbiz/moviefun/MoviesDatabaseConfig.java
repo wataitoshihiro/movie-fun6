anusha-vataparambu/apps-movie-fun-code

Implement two data sources

src/main/java/org/superbiz/moviefun/MoviesDatabaseConfig.java
@@ -1,0 +1,42 @@
+package org.superbiz.moviefun;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+public class MoviesDatabaseConfig {
+
+    @Bean
+    public DataSource moviesDataSource(DatabaseServiceCredentials serviceCredentials) {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(serviceCredentials.jdbcUrl("movies-mysql", "p-mysql"));
+        HikariConfig config = new HikariConfig();
+        config.setDataSource(dataSource);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean moviesEntityManagerFactory(DataSource moviesDataSource, HibernateJpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(moviesDataSource);
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        factoryBean.setPackagesToScan(DatabaseConfig.class.getPackage().getName());
+        factoryBean.setPersistenceUnitName("movies");
+        return factoryBean;
+    }
+
+    @Bean
+    PlatformTransactionManager moviesTransactionManager(EntityManagerFactory albumsEntityManagerFactory) {
+        return new JpaTransactionManager(albumsEntityManagerFactory);
+    }
+}

wataitoshihiro/movie-fun6

src/main/java/org/superbiz/moviefun/MoviesDatabaseConfig.java
@@ -1,0 +1,42 @@
+package org.superbiz.moviefun;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+public class MoviesDatabaseConfig {
+
+    @Bean
+    public DataSource moviesDataSource(DatabaseServiceCredentials serviceCredentials) {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(serviceCredentials.jdbcUrl("movies-mysql"));
+        HikariConfig config = new HikariConfig();
+        config.setDataSource(dataSource);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean moviesEntityManagerFactory(DataSource moviesDataSource, HibernateJpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(moviesDataSource);
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        factoryBean.setPackagesToScan(DatabaseConfig.class.getPackage().getName());
+        factoryBean.setPersistenceUnitName("movies");
+        return factoryBean;
+    }
+
+    @Bean
+    PlatformTransactionManager moviesTransactionManager(EntityManagerFactory moviesEntityManagerFactory) {
+        return new JpaTransactionManager(moviesEntityManagerFactory);
+    }
+}